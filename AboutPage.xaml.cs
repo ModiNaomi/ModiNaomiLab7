ff52f1a5a8f7f2ef173e225892f6eb20d0fb80bb
NAOMI-ESTERA MODI <naomi.modi@stud.ubbcluj.ro>
2025-01-11 17:38:58 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ModiNaomiLab7;
+
+public partial class AboutPage : ContentView
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ModiNaomiLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
